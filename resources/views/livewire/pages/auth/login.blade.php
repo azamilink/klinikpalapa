ae7c2c13876209b13581ab8ce2251320a1ffcb99
azamilink <zamiaswad@gmail.com>
2025-01-15 21:24:10 +0700

    Auth Pages

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -23,7 +23,7 @@ new #[Layout('layouts.guest')] class extends Component {
     }
 }; ?>
 
-<div>
+<div class="container mx-auto max-w-md mt-12">
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
