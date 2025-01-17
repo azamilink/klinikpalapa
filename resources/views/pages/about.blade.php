409142f3313f7d719aaba2fc2b6e073a872eb797
azamilink <zamiaswad@gmail.com>
2025-01-18 00:04:36 +0700

    Menambahkan Backgroun Image di about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -86,49 +86,50 @@
     {{-- Penanggung Jawab End --}}
 
     {{-- Sejarah Start --}}
-    <section class="container mx-auto py-12">
-        <div class="grid grid-cols-1 justify-items-center my-4">
-            <h2 class="text-pretty text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
-                Sejarah Kami
-            </h2>
-            <p class="mt-6 text-lg/8 text-gray-600">Awal Mula Palapa Dentist Berdiri.</p>
+    <section class="w-full min-h-screen py-12" style="background-image: url({{ asset('images/about/background.jpg') }})">
+        <div class="container mx-auto">
+            <div class="grid grid-cols-1 justify-items-center my-4">
+                <h2 class="text-pretty text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
+                    Sejarah Kami
+                </h2>
+                <p class="mt-6 text-lg/8 text-gray-600">Awal Mula Palapa Dentist Berdiri.</p>
+            </div>
+
+
+            <ul class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical max-w-4xl mx-auto mt-2">
+                <li>
+                    <div class="timeline-middle">
+                        <div class="avatar">
+                            <div class="w-24 rounded-full">
+                                <img src="https://palapadentists.com/wp-content/uploads/2019/01/Icon-Foto-drg.-Rani.png" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline-start mb-10 md:text-end">
+                        <time class="font-mono italic">2007</time>
+                        <div class="text-lg font-black">- Kelanjutan</div>
+                        Setelah wafatnya drg. Solhah Busro ditahun 2007, kelanjutan klinik diteruskan dan dikembangkan oleh putri beliau Dr. drg. Anggraeni Sp.KG. (drg. Rani) yang merupakan salah satu alumnus terbaik Fakultas Kedokteran Gigi Universitas Indonesia.
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div class="timeline-middle">
+                        <div class="avatar">
+                            <div class="w-24 rounded-full">
+                                <img src="https://palapadentists.com/wp-content/uploads/2019/01/Icon-Foto-drg.-Solhah.png" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline-end md:mb-10">
+                        <time class="font-mono italic">1980</time>
+                        <div class="text-lg font-black">- Mulai Berdiri</div>
+                        Pada awalnya, klinik ini merupakan bagian dari rumah pribadi drg. Solhah Busro yang terletak di Jl. Palapa 1 No. 4, Pasar Minggu Jakarta Selatan. drg. Solhah Busro merupakan dokter gigi yang juga sekaligus dosen di Fakultas Kedokteran Gigi Universitas Trisakti.
+                    </div>
+                    <hr />
+                </li>
+            </ul>
         </div>
-
-
-        <ul class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical max-w-4xl mx-auto mt-2">
-            <li>
-                <div class="timeline-middle">
-                    <div class="avatar">
-                        <div class="w-24 rounded-full">
-                            <img src="https://palapadentists.com/wp-content/uploads/2019/01/Icon-Foto-drg.-Rani.png" />
-                        </div>
-                    </div>
-                </div>
-                <div class="timeline-start mb-10 md:text-end">
-                    <time class="font-mono italic">2007</time>
-                    <div class="text-lg font-black">- Kelanjutan</div>
-                    Setelah wafatnya drg. Solhah Busro ditahun 2007, kelanjutan klinik diteruskan dan dikembangkan oleh putri beliau Dr. drg. Anggraeni Sp.KG. (drg. Rani) yang merupakan salah satu alumnus terbaik Fakultas Kedokteran Gigi Universitas Indonesia.
-                </div>
-                <hr />
-            </li>
-            <li>
-                <hr />
-                <div class="timeline-middle">
-                    <div class="avatar">
-                        <div class="w-24 rounded-full">
-                            <img src="https://palapadentists.com/wp-content/uploads/2019/01/Icon-Foto-drg.-Solhah.png" />
-                        </div>
-                    </div>
-                </div>
-                <div class="timeline-end md:mb-10">
-                    <time class="font-mono italic">1980</time>
-                    <div class="text-lg font-black">- Mulai Berdiri</div>
-                    Pada awalnya, klinik ini merupakan bagian dari rumah pribadi drg. Solhah Busro yang terletak di Jl. Palapa 1 No. 4, Pasar Minggu Jakarta Selatan. drg. Solhah Busro merupakan dokter gigi yang juga sekaligus dosen di Fakultas Kedokteran Gigi Universitas Trisakti.
-                </div>
-                <hr />
-            </li>
-        </ul>
-
     </section>
     {{-- Sejarah End --}}
 
